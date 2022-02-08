578a4b3e4279a7528b5bd43323295e9528345db9
Nguyễn Hữu Toàn <mac@huutoan.domain.com>
2022-02-08 09:43:07 +0700

    create api signup & signin for admin

---
+++ controller/admin/isAuth.js
@@ -0,0 +1,88 @@
+const User = require("../../models/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const shortid = require("shortid");
+
+module.exports = {
+  signup: async (req, res) => {
+    try {
+      // get admin info
+      const { firstName, lastName, email, password, username } = req.body;
+
+      const user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({
+          msg: "Admin already registered",
+        });
+      }
+
+      const hash_password = await bcrypt.hash(password, 10);
+
+      const newAdmin = new User({
+        firstName,
+        lastName,
+        email,
+        password: hash_password,
+        username: shortid.generate(),
+        role: "admin",
+      });
+
+      newAdmin.save();
+
+      return res.status(200).json({
+        msg: "Admin created Successfully..!",
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  signin: async (req, res) => {
+    try {
+      const email = req.body.email;
+      const password = req.body.password;
+
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        res.status(400).json({ msg: "Wrong" });
+      }
+
+      if (user) {
+        const isPassword = await user.authenticate(password);
+
+        if (isPassword && user.role == "admin") {
+          const accessToken = jwt.sign(
+            {
+              _id: user._id,
+              role: user.role,
+            },
+            process.env.JWT_SECRET,
+            {
+              expiresIn: "1d",
+            }
+          );
+          const { _id, firstName, lastName, email, role, fullName } = user;
+          res.cookie("token", accessToken, { expiresIn: "1d" });
+          res.status(200).json({
+            accessToken,
+            user: { _id, firstName, lastName, email, role, fullName },
+          });
+        } else {
+          return res.status(400).json({
+            message: "Invalid Password",
+          });
+        }
+      } else {
+        return res.status(400).json({ message: "Something went wrong" });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  logout: (req, res) => {
+    res.clearCookie("token");
+    res.status(200).json({
+      message: "Signout successfully...!",
+    });
+  },
+};
--- routers/user.js
+++ routers/user.js
@@ -2,6 +2,7 @@ const { register, login } = require("../controller/user.controller");
 const { requireSignin } = require("../middleware/auth");
 const category = require("../controller/category.controller");
 const express = require("express");
+const { signup, signin, logout } = require("../controller/admin/isAuth");
 
 const router = express.Router();
 
@@ -10,6 +11,9 @@ router.post("/login", login);
 router.post("/profile", requireSignin, (req, res) => {
   res.status(200).json({ msg: "profile" });
 });
+
+router.post("/admin/signup", signup);
+router.post("/admin/signin", signin);
 
 router.post("/category", category);
 
